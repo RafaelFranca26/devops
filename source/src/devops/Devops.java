f6d509bc6a0e86afbd82fc6ae546ea0e07495b43
visitante <visitante@IFAL.LOCAL>
2018-04-28 10:04:48 -0300

    teste do waffle.io #1

--- source/src/devops/Devops.java
+++ source/src/devops/Devops.java
@@ -17,6 +17,7 @@ public class Devops {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello world2");
+        System.out.println("Hello world3");
     }
     
 }
