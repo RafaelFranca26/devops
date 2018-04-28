698200ad1c64320a318af8a5fbc7604f0d3752d7
visitante <visitante@IFAL.LOCAL>
2018-04-28 09:44:51 -0300

    Segundo commit

--- source/src/devops/Devops.java
+++ source/src/devops/Devops.java
@@ -15,7 +15,8 @@ public class Devops {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        System.out.println("Hello world");
+        System.out.println("Hello world2");
     }
     
 }
